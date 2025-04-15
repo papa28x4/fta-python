20ab7995734107e91d339b7791e89676ca21c80d
papa28x4 <swissfair@yahoo.co.uk>
2025-04-15 20:14:25 +0100

    add introduction

---
+++ lessons/introduction.py
@@ -0,0 +1,11 @@
+# Python is a high-level, multipurpose programming language created by Guido van Rossum and released in 1991.
+
+# Python is a very popular choice for machine learning, data analysis, web development (server-side),
+# software development and task automation or scripting.
+
+# Python was designed to be easily readable with it sharing some similarities to the English Language.
+# The end of a line marks the end of a statement, unlike some other programming languages, Python does not require a
+# semicolon at the end of each statement. Python relies on indentation, to define scope; such as the scope of loops,
+# functions and classes. Other programming languages often use curly-brackets for this purpose.
+
+# Python files use the ".py" extension. The most recent version of Python is Python 3.
