8f7b656d051a6e1f28d385e07e20e7207c5d1efd
papa28x4 <swissfair@yahoo.co.uk>
2025-04-18 14:30:31 +0100

    update datatypes

--- lessons/string-formatting.py
+++ lessons/string-formatting.py
@@ -2,6 +2,71 @@
 Python String Formatting
 '''
 
+# It is a common routine to concatenate strings with variables. Python offers several ways to achieve this
+
+''' Method 1 '''
+### The + operator ###
+first_name = "Jamie"
+last_name = "Foxx"
+full_greeting = "Hello, " + first_name + " " + last_name + "!"
+
+print('full greeting from + operator:', full_greeting)  # Hello, Jamie Foxx!
+
+# If you attempt to use the + operator on a string and an integer, you will get an error
+# print("8" + 8)  # TypeError: can only concatenate str (not "int") to str
+
+# The above behaviour is different from what Javascript would do. It won't throw an error. Rather Javascript will do a
+# type coercion converting the integer to a string and concatenate them.
+# console.log("8" + 8)  # 88
+
+
+''' Method 2'''
+### Using the % operator ###
+# % is the modulus operator when applied on integers but when applied on a string, % becomes the format operator.
+# The format operator uses symbols like %d, %f, %s etc as placeholders that would be eventually  replaced with
+# the data stored in variables. Although it's the old way of doing string formatting, it's still
+# important to understand it because you might come across it in legacy code.
+
+# If there is just one replacement variable, you don't need brackets
+count = 36
+fact = "Nigeria has %d states" % count
+print("Naija fact:", fact)  # Nigeria has 36 states
+
+# If there is more than one replacement variable, they should be enclosed in brackets separated by commas.
+# The replacement is dependent on the order of the variables. The first variable (first_name) will replace the first %s,
+# while the second variable will replace the second %s.
+
+full_greeting = "Hello, %s %s!" % (first_name, last_name)
+print('full greeting from % operator:', full_greeting)  # Hello, Jamie Foxx!
+
+
+# name='TutorialsPoint'
+# print ('Welcome To %.5s The largest Tutorials Library' % (name, ))
+
+
+''' Method 3'''
+# The format method
+# The format method uses curly bracket {} as placeholders. There are 3 ways to apply the format method:
+
+# Using empty placeholders
+biodata = "My name is {}, I'm {}".format("John", 36)
+print('biodata1:', biodata)  # prints => My name is John, I'm 36
+
+# Using numbered indexes
+# string.format(0, 1, 2 ... )
+biodata = "My name is {0}, I'm {1}".format("John", 36)
+print('biodata2:', biodata)  # prints => My name is John, I'm 36
+
+choice = "I prefer {1} to {0}".format("apples", "grapes")
+print("My choice: ", choice)  # prints => I prefer grapes to apples
+
+# Using named indexes
+biodata = "My name is {fname}, I'm {age}".format(fname="John", age=36)
+print('biodata3:', biodata)  # prints => My name is John, I'm 36
+
+
+
+''' Method 4'''
 # This is the preferred way to format a string. This feature is available in python 3.6 and above
 # To use f-string, put an f in front of the string like this:
 
--- lessons/variables_and_datatypes.py
+++ lessons/variables_and_datatypes.py
@@ -45,15 +45,15 @@ print(score1)  # prints 80
 print(score2)  # prints 80
 print(score3)  # prints 80
 
-''' Swapping Variables'''
+''' Swap Variables'''
 
 pos_one = 1
 pos_two = 2
 
 pos_one, pos_two = pos_two, pos_one
 
-print('pos_one after swap:', pos_one)
-print('pos_two after swap:', pos_two)
+print('pos_one after swap:', pos_one)  # prints 2
+print('pos_two after swap:', pos_two)  # prints 1
 
 
 '''
@@ -71,19 +71,18 @@ print('pos_two after swap:', pos_two)
 
 
 number1 = 40
-print(number1, 'is of type', type(number1)) # prints <class 'int'>
+print(number1, 'is of type', type(number1))  # prints <class 'int'>
 
 number2 = 8.0
-print(number2, 'is of type', type(number2)) # prints <class 'float'>
+print(number2, 'is of type', type(number2))  # prints <class 'float'>
 
 number3 = 1+4j
-print(number3, 'is of type', type(number3)) # prints <class 'complex'>
+print(number3, 'is of type', type(number3))  # prints <class 'complex'>
 
 
 ################################## String ################################################################
 
 # String holds sequence of characters. It is enclosed with single quote or double quotes
-
 
 sentence = 'I am string'
 
@@ -93,10 +92,10 @@ print(sentence, 'is of type', type(sentence))  # prints => <class 'str'>
 #################################### Boolean #############################################################
 
 # This represents one of the two values: True or False
-# Notice the boolean values start with uppercase unlike Javascript where it is true or false
+# Notice the boolean values start with uppercase whereas in Javascript it is true or false
 
 a = True
-print('True is of type:',type(a))  # prints => <class 'bool'>
+print('True is of type:', type(a))  # prints => <class 'bool'>
 
 b = False
 print('False is of type:', type(b))  # prints => <class 'bool'>
@@ -104,20 +103,17 @@ print('False is of type:', type(b))  # prints => <class 'bool'>
 # Each time you compare two values, Python returns a boolean
 score1 = 40
 score2 = 30
-print('Is score1 less than score2?', score1 < score2)
-print('Is score1 equal to score2?', score1 == score2)
-print('Is score1 more than score2?', score1 > score2)
+print('Is score1 less than score2?', score1 < score2)  # False
+print('Is score1 equal to score2?', score1 == score2)  # False
+print('Is score1 more than score2?', score1 > score2)  # True
 
 ######### Truthy and Falsy ###########
-# Truthy and Falsy refer to values of other data types that evaluate to true and false respectively when evaluated in
-# a boolean context. The bool function is used to coerce other data types into a boolean
+# Truthy and Falsy refer to values of other data types that evaluate to True and False respectively when evaluated in
+# a boolean context. The bool function is used to coerce other data types into a boolean.
 
 # Most Values are True (Truthy)
-#
 # Any string is True, except empty strings.
-#
 # Any number is True, except 0.
-#
 # Any list, tuple, set, and dictionary are True, except empty ones.
 
 print("empty string =>", bool(''))  # Falsy
@@ -127,10 +123,50 @@ print("empty tuple => ", bool(()))  # Falsy
 print("empty set => ", bool(set()))  # Falsy
 print("empty dict => ", bool({}))  # Falsy
 
+
 ##################################### List ################################################################
+# A list is an ORDERED collection of items. A list has items separated by comma wrapped in square brackets. It shares a
+# lot of similarities with the array in Javascript
+
+list_example = ["BMW", "Benz", "Volvo", "Toyota", "Audi"]
+
+# Because they are ordered, they can be accessed with the index number starting from zero
+print('first item in list_example', list_example[0])
+print('second item in list_example', list_example[1])
+print('third item in list_example', list_example[2])
+
+print(sentence, 'is of type', type(sentence))  # prints => <class 'str'>
 
 
+# We delve deeper into lists in a future lesson
 
-# Sequence => list, tuple, range (holds collection of items)
-# Boolean => bool (holds either True or False)
-# Set => set (holds collection of unique items)
+
+##################################### Tuple ################################################################
+# Like a list, a tuple is an ordered collection of items. The difference between them is that a tuple is immutable.
+# This makes tuples a good choice when you want the data in your collection to be read-only and constant.
+# A Tuple is denoted by comma separated values enclosed in parentheses.
+
+# Create an empty tuple
+tuple1 = ()
+
+# A tuple with content
+tuple2 = ("hello", "world")
+
+# Let's see an example of the immutability of tuples
+my_list = ["One", "Three", "Five", "Seven"]
+my_tuple = ("One", "Three", "Five", "Seven")
+
+my_list[1] = "Eight"
+print('updated my_list', my_list)  # ['One', 'Eight', 'Five', 'Seven']
+
+# If you try to do the same with a tuple, you get a type error
+# my_tuple[1] = "Eight"  # TypeError: 'tuple' object does not support item assignment (Uncomment and run to see error)
+
+# Like lists, tuples can be accessed using the index number
+print('To get the first item from left to right:', my_tuple[0])  # One
+print('To get the last item or the first from the right to left:', my_tuple[-1])  # Seven
+
+
+##################################### Set ################################################################
+
+# A set holds a collection of unique items. Duplicates are not allowed.
