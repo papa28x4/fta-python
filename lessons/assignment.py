ca0f8f2092eefc225a73f218769105f82e5e965f
dammyloye <dammy.loye@yahoo.com>
2025-06-01 20:31:50 +0100

    Update assignment.py

--- lessons/assignment.py
+++ lessons/assignment.py
@@ -1,30 +1,37 @@
 ''' (1) '''
 # Given the below url, use the strip method to get "example"
 url = 'www.example.com'
+result = url.strip('w.com')
+print(result)  # Output: 'example'
 # Expected result => 'example'
-
 
 ''' (2) '''
 # Given the following string:
 sentence = 'Aunty Aunika is the Author'
+result = sentence.strip('Aunty')
+print(result)
 # Expected result => 'Aunika is the Author'
 
 
 ''' (3) '''
 # Replace 's' with '$'. Only replace the first 2
 sample = 'Recessions'
+result = sample.replace('s', '$', 2)
+print(result)
 # Expected result => Rece$$ions
 
 
 ''' (4) '''
 # Write a program to transform a word to its continuous tense
 word = 'drive'
+result = word.replace('e', '') + 'ing'
+print(result)
 # Expected result => driving
 
 
 ''' (5) '''
 price = 2000
-txt = f'The price is {price} dollars'
+txt = f'The price is {price:,} dollars'
 print(txt)
 
 # Use Modifiers to get the result below
